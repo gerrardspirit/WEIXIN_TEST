07b2626ae9da54193ca2450cfd88f636160c9c5e
Gerrard <gerard.chen@innocellence.com>
2015-07-08 11:18:08 +0800

    test

--- WeChat_Jeff/WeChat/Controllers/HomeController.cs
+++ WeChat_Jeff/WeChat/Controllers/HomeController.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WeChat.Controllers {
-    public class HomeController : Controller {
-        public ActionResult Index() {
-            ViewBag.Title = "Home Page";
+namespace WeChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            ViewBag.Title = "Home Page" + id;
 
             return View();
         }
